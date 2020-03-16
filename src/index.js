8a2a3b805305392628fa47f6c6da1ff33a3fc963
Ulyana Hubich <ulyana.hubich@gmail.com>
2020-03-16 16:52:11 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let brackets = [];
+  let i = 0;
+
+  while (i < bracketsConfig.length) {
+    brackets = bracketsConfig[i].join('');
+
+    if (str.includes(brackets)) {
+      str = str.replace(brackets,'');
+      i = 0;
+
+    } else {
+      i++;
+    }
+  }
+
+  if (str.length == 0) {
+      return true;
+    } else 
+    return false;
 }
